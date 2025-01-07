41c996cfbc850290e6c0b0f577d877e7f84bf3fc
mssid <3485108+mssid@users.noreply.github.com>
2025-01-07 12:45:27 +0000

    blacked

--- web_app/main.py
+++ web_app/main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/status")
 def hello_world():
     return "OK"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
